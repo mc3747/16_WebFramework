842ebdf1f1ceea142619ce530f7fcf7460ebdef6
mc3747 <mc3747@126.com>
2019-12-26 18:04:21 +0800

    Update tool.js

--- react-summary/src/tool/tool.js
+++ react-summary/src/tool/tool.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import colorTool from './toolList/colorTool';
-import stringTool from './toolList/stringTool';
-import regexTool from './toolList/regexTool';
+import ColorTool from './toolList/colorTool';
+import { getCurrent, getTimeStamp, getDateDiff } from './toolList/dateTool';
+import { toLine } from './toolList/stringTool';
+import { extripHttps } from './toolList/regexTool';
+// import stringTool from './toolList/stringTool';
+// import regexTool from './toolList/regexTool';
 
-var dateTool = require('./toolList/dateTool');
 export default class Tool extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        let reg = new RegExp(extripHttps);
+
         return (
             <div>
                 <div style={{
-                    color: colorTool.getRandomColorString()
+                    color: ColorTool.getRandomColorString()
                 }}>1,颜色</div>
-                <div>2,日期：{dateTool.getCurrent}</div>
-
+                <div>2,日期：{getDateDiff('2019-12-12', '2023-01-01')}</div>
+                <div>3,字符串：{toLine('myBestFriend')}</div>
+                <div>4,正则：{('https://www.baidu.com').match(reg)}</div>
             </div>
 
         )
