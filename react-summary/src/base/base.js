f13fa28d076c5cc2c074d7693908dd447a82ace8
马成 <mc3747@126.com>
2020-08-20 23:37:33 +0800

    文件

--- react-summary/src/base/base.js
+++ react-summary/src/base/base.js
@@ -2,14 +2,16 @@ import React, { Component,useState } from 'react';
 import { Button } from 'antd-mobile';
 
 /**
- * 1,html,
- * 2,css,
+ * 1,html
+ * 2,css
  * 3,js基础
  * 4.异步
- * 5.网络
+ * 5.网络:ajax
  * 6.存储
  * 7,dom
  * 8.bom
+ * 9.canvas
+ * 10.
  * 
  *  */ 
 
