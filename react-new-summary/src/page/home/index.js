0bd33d9450bd2da92086e984f4613fcc9de3109f
小马 <mc3747@126.com>
2021-03-27 15:49:46 +0800

    更新

--- react-new-summary/src/page/home/index.js
+++ react-new-summary/src/page/home/index.js
@@ -8,11 +8,17 @@ class Home extends Component {
             <div className="Word-header">
                 <Header param="home的"/>
                 <h1>Home page</h1>
-                <button onClick={()=>{this.gotoHome('/home')}}>跳转Home页</button>
+                <button onClick={()=>{this.gotoHome('/home')}}>跳转Home页1</button>
+                <button onClick={()=>{this.gotoHome.bind(this,'/home')}}>跳转Home页2</button>
+                <button onClick={()=>{e=>this.gotoHome(e,'/home')}}>跳转Home页3</button>
             </div>
         )
     }
     gotoHome=(a)=> {
+        console.log(a)
+        this.props.history.push(a)
+    }
+    gotoHome=(e,a)=> {
         console.log(a)
         this.props.history.push(a)
     }
